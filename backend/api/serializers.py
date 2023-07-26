8b3d353def33b65f4ee7b26f16586e2170fbc07b
SimoneVita <vitalysimonenko@yandex.ru>
2023-07-26 14:27:52 +0300

    pep-8-1

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -64,14 +64,14 @@ class RecipeSerializer(ModelSerializer):
         return (request.user.is_authenticated
                 and Favorite.objects.filter(user=request.user,
                                             recipe=obj).exists()
-        )
+                )
 
     def get_is_in_shopping_cart(self, obj):
         request = self.context.get('request')
         return (request.user.is_authenticated
                 and ShoppingCart.objects.filter(user=request.user,
                                                 recipe=obj).exists()
-        )
+                )
 
     def validate(self, data):
         user = self.context.get('request').user
