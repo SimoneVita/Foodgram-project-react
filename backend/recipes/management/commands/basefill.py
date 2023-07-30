9a04d0a4fdbc41db1d2205d602ff82bac7e99c35
SimoneVita <vitalysimonenko@yandex.ru>
2023-07-30 16:15:21 +0300

    Some extra

--- backend/recipes/management/commands/basefill.py
+++ backend/recipes/management/commands/basefill.py
@@ -1,27 +1,27 @@
 import csv
-import os
+# import os
 
-from django.core.management.base import BaseCommand
-
-from foodgram import settings
+from django.core.management import BaseCommand
 from recipes.models import Ingredient
+
+# FILES_DIR = os.path.dirname(os.path.dirname(os.getcwd()))
 
 
 class Command(BaseCommand):
-    help = 'Импорт данных из csv файла'
+    help = "import data from ingredients.csv"
 
-    def handle(self, *args, **options):
-        print('Начинаю заполнять базу.')
-        path = os.path.join(settings.BASE_DIR, 'ingredients.csv')
-        with open(path, 'r') as csv_file:
-            reader = csv.reader(csv_file)
-            for row in reader:
-                try:
-                    Ingredient.objects.get_or_create(
-                        name=row[0],
-                        measurement_unit=row[1],
-                    )
-                except Exception as error:
-                    print(f'Ошибка в строке {row}: {error}')
+    def handle(self, *args, **kwargs):
+        # with open(f'{FILES_DIR}/foodgram-project-react/data/ingredients.csv',
+        #           'r', encoding='utf-8') as file:
+        with open('recipes/data/ingredients.csv',
+                  'r', encoding='utf-8') as file:
+            file_reader = csv.reader(file)
+            next(file_reader)
 
-        print('База заполнена.')
+            for row in file_reader:
+                name, measurement_unit = row
+                Ingredient.objects.get_or_create(
+                    name=name,
+                    umeasurement_unit=measurement_unit
+                )
+        self.stdout.write(self.style.SUCCESS('Ингредиенты добавлены'))
