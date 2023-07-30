6af02505576bc14f0af3b66c017ef14113a78a6b
SimoneVita <vitalysimonenko@yandex.ru>
2023-07-30 04:13:28 +0300

    changes for project

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,7 +7,6 @@ from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-
 from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
                             ShoppingCart, Tag, TagsRecipes)
 
